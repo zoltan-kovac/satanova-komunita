a7a236626f14d2edf9ec48e643ea2790446ee34c
zkovac <zolo.kovac@gmail.com>
2019-12-02 12:29:34 +0100

    redirect to fb after fixing broken stuff

--- src/pages/o-nas/index.jsx
+++ src/pages/o-nas/index.jsx
@@ -12,7 +12,7 @@ import Constitution from './Constitution'
 
 export default ({ location }) => {
   const { site: { siteMetadata } } = useStaticQuery(graphql
-    `query AboutUsQuery{asdasd
+    `query AboutUsQuery{
       site {
         siteMetadata {
           title
